2c6837ab7c737e4b34307ecad38167f1135b8fbb
prabodhaSA <prabo918@gmail.com>
2020-09-23 11:00:28 +0530

    insert meal

--- app/src/main/java/com/example/mad_y2s2/Update_meal.java
+++ app/src/main/java/com/example/mad_y2s2/Update_meal.java
@@ -2,7 +2,10 @@ package com.example.mad_y2s2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Update_meal extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Update_meal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_meal);
+
+        Button bt1 = findViewById(R.id.btn_tot_cal);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Update_meal.this, activity_total_food_calories_count.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
