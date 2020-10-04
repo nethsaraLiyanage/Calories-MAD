06bb6ca360fc5a2f0360fd006e9ce9bcbd1e9677
alphachamod <34671487+alphachamod@users.noreply.github.com>
2020-10-04 14:38:49 +0530

    Login redirection added. Added BMI Calculator to bottom navigation bar,

--- app/src/main/java/com/example/mad_y2s2/SignupActivity.java
+++ app/src/main/java/com/example/mad_y2s2/SignupActivity.java
@@ -38,32 +38,55 @@ public class SignupActivity extends AppCompatActivity {
         signUp = findViewById(R.id.signup_button);
 
 
-        std = new Users();
 
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //openHome();
-            dbRef = FirebaseDatabase.getInstance().getReference().child("Users");
+                std = new Users();
+                dbRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+                String full_name = name.getText().toString();
+                String user_email = email.getText().toString();
+                String user_username = username.getText().toString();
+                String user_password = password.getText().toString();
+                String user_weight = weight.getText().toString();
+                String user_height = height.getText().toString();
+                String user_signup = signUp.getText().toString();
+
+
                 try {
-                    if(TextUtils.isEmpty(name.getText().toString())){
-                        Toast.makeText(getApplicationContext(),"Name ia Empty", Toast.LENGTH_SHORT).show();
+                    if( full_name.isEmpty()){
+
+                        name.setError("Enter Your Name");
+                        name.requestFocus();
                     }
-                    else if(TextUtils.isEmpty(email.getText().toString())){
-                        Toast.makeText(getApplicationContext(),"Name ia Empty", Toast.LENGTH_SHORT).show();
+                    else if( user_email.isEmpty()){
+
+                        email.setError("Enter Your Email");
+                        email.requestFocus();
                     }
-                    else if(TextUtils.isEmpty(username.getText().toString())){
-                        Toast.makeText(getApplicationContext(),"Name ia Empty", Toast.LENGTH_SHORT).show();
+                    else if( user_username.isEmpty()){
+
+                        username.setError("Enter Your Username");
+                        username.requestFocus();
                     }
-                    else if(TextUtils.isEmpty(password.getText().toString())){
-                        Toast.makeText(getApplicationContext(),"Name ia Empty", Toast.LENGTH_SHORT).show();
+                    else if( user_password.isEmpty()){
+
+                        password.setError("Enter Your Password");
+                        password.requestFocus();
                     }
-                    else if(TextUtils.isEmpty(weight.getText().toString())){
-                        Toast.makeText(getApplicationContext(),"Name ia Empty", Toast.LENGTH_SHORT).show();
+                    else if( user_weight.isEmpty()){
+
+                        weight.setError("Enter Your Weight");
+                        weight.requestFocus();
                     }
-                    else if(TextUtils.isEmpty(height.getText().toString())){
-                        Toast.makeText(getApplicationContext(),"Name ia Empty", Toast.LENGTH_SHORT).show();
+                    else if( user_height.isEmpty()){
+
+
+                        height.setError("Enter Your Height");
+                        height.requestFocus();
                     }
                     else{
 
@@ -74,9 +97,11 @@ public class SignupActivity extends AppCompatActivity {
                         std.setWeight(weight.getText().toString().trim());
                         std.setHeight(height.getText().toString().trim());
 
-                        dbRef.push().setValue(std);
+                        dbRef.child(""+username.getText().toString()).setValue(std);
+
                         Toast.makeText(getApplicationContext(),"Successfully Registered", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(),activity_login.class);
+                        clearControls();
+                        Intent intent = new Intent(getApplicationContext(),LoginUsers.class);
                         startActivity(intent);
                     }
                 }
@@ -91,18 +116,22 @@ public class SignupActivity extends AppCompatActivity {
         toLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openlogIn();
+                Intent object =  new Intent(getApplicationContext(), LoginUsers.class);
+                startActivity(object);
             }
-        });
+        }
+        );
     }
 
-    private void openHome(){
-        Intent homeIntent = new Intent(this, activity_home.class);
-        startActivity(homeIntent);
+    private void clearControls(){
+
+        name.setText("");
+        email.setText("");
+        username.setText("");
+        password.setText("");
+        weight.setText("");
+        height.setText("");
     }
 
-    private void openlogIn(){
-        Intent loginIntent = new Intent(this, activity_login.class);
-        startActivity(loginIntent);
-    }
+
 }
